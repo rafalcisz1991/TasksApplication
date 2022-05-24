f912508db4ed9b4e03e9c63c462d9ef36894c96a
rafalcisz1991 <rafal.cisz1991@gmail.com>
2022-05-24 21:46:10 +0200

    Zadanie 29.1 - poprawki testów

--- src/test/java/com/crud/tasks/trello/mapper/TrelloMapperTest.java
+++ src/test/java/com/crud/tasks/trello/mapper/TrelloMapperTest.java
@@ -18,7 +18,7 @@ class TrelloMapperTest {
 
     //mapToBoards() test
     @Test
-    public void shouldFetchBoardsList() {
+    void shouldFetchBoardsList() {
         //Given
         ArrayList<TrelloBoardDto> boardDtoArrayList = new ArrayList<>();
         TrelloBoardDto trelloBoardDto1 = new TrelloBoardDto("name1", "1", new ArrayList<>());
@@ -46,4 +46,37 @@ class TrelloMapperTest {
         assertEquals(trelloBoard.getId(), "1");
         assertEquals(trelloBoard2.getId(), "2");
     }
+
+    @Test
+    void shouldFetchBoardsDtoList() {
+        //Given
+        ArrayList<TrelloBoard> trelloBoards = new ArrayList<>();
+        TrelloBoard trelloBoard1 = new TrelloBoard("1", "name1", new ArrayList<>());
+        TrelloBoard trelloBoard2 = new TrelloBoard("2", "name2", new ArrayList<>());
+
+        //When
+        trelloBoards.add(trelloBoard1);
+        trelloBoards.add(trelloBoard2);
+        List<TrelloBoardDto> trelloBoardDtos = trelloMapper.mapToBoardsDto(trelloBoards);
+
+        //Then
+        TrelloBoardDto trelloBoardDto1 = trelloBoardDtos.get(0);
+        TrelloBoardDto trelloBoardDto2 = trelloBoardDtos.get(1);
+
+        assertTrue(trelloBoardDtos.contains(trelloBoardDto1));
+        assertTrue(trelloBoardDtos.contains(trelloBoardDto2));
+        assertFalse(trelloBoardDtos.isEmpty());
+        assertNotNull(trelloBoardDto1.getLists());
+        assertNotNull(trelloBoardDto2.getLists());
+        assertEquals(2, trelloBoardDtos.size());
+        assertTrue(trelloBoardDto1.getLists().isEmpty());
+        assertTrue(trelloBoardDto2.getLists().isEmpty());
+        assertEquals(trelloBoardDto1.getName(), "name1");
+        assertEquals(trelloBoardDto2.getName(), "name2");
+        assertEquals(trelloBoardDto1.getId(), "1");
+        assertEquals(trelloBoardDto2.getId(), "2");
+
+    }
+
+
 }
